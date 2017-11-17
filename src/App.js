1ba1303becb40398c06f3ae91656d6c35ecae65a
baxtergu <baxtergu@gmail.com>
2017-11-17 10:26:24 +0800

    初始化项目开始状态

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const contacts = [
+  { name: 'Lucy', gender: 'male', age: 30, introduction: 'Developer' },
+  { name: 'Lily', gender: 'female', age: 28, introduction: 'Painter' },
+  { name: 'Lilei', gender: 'male', age: 35, introduction: 'English Teacher' },
+  { name: 'John', gender: 'female', age: 24, introduction: 'Web Designer' }
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        {contacts.map(this.renderContact)}
+      </div>
+    );
+  }
+
+  renderContact(contact) {
+    return (
+      <div className={`contact contact--${contact.gender}`}>
+        <h2 className="contact__name">{contact.name}</h2>
+        <p className="contact__intro">
+          {contact.name} is a <strong>{contact.age} years</strong> old
+          <strong> {contact.introduction}</strong>
         </p>
       </div>
     );
