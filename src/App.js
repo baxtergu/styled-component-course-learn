3ab39c00b0f08985b1cc5dbb86753baf7cafc7a2
baxtergu <baxtergu@gmail.com>
2017-11-17 10:50:32 +0800

    STEP-03

--- src/App.js
+++ src/App.js
@@ -1,56 +1,23 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import './App.css';
+// import Contact from './components/Contact';
+import PasswordInput from './components/PasswordInput';
 
 const contacts = [
-  { name: 'Lucy', gender: 'male', age: 30, introduction: 'Developer' },
-  { name: 'Lily', gender: 'female', age: 28, introduction: 'Painter' },
-  { name: 'Lilei', gender: 'male', age: 35, introduction: 'English Teacher' },
-  { name: 'John', gender: 'female', age: 24, introduction: 'Web Designer' }
+  // { name: 'Lucy', gender: 'male', age: 30, introduction: 'Developer' },
+  // { name: 'Lily', gender: 'female', age: 28, introduction: 'Painter' },
+  // { name: 'Lilei', gender: 'male', age: 35, introduction: 'English Teacher' },
+  // { name: 'John', gender: 'female', age: 24, introduction: 'Web Designer' }
 ];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {contacts.map(this.renderContact)}
+        <PasswordInput name="pass1" />
+        <PasswordInput name="pass2" size="2em" />
       </div>
     );
   }
-
-  renderContact(contact) {
-    return (
-      <Contact gender={contact.gender}>
-        <ContactName>{contact.name}</ContactName>
-        <ContactIntro>
-          {contact.name} is a <strong>{contact.age} years</strong> old
-          <strong> {contact.introduction}</strong>
-        </ContactIntro>
-      </Contact>
-    );
-  }
 }
-
-const Contact = styled.div`
-  border: 1px solid #DEDEDE;
-  border-radius: 5px;
-  padding: 15px;
-  margin: 15px;
-  background-color: ${props => (props.gender === 'female' ? 'tomato' : 'green')};
-  color: white;
-`;
-
-const ContactName = styled.h2`
-  font-size: 20px;
-  margin-top: 0;
-`;
-
-const ContactIntro = styled.p`
-  font-size: 16px;
-  margin-bottom: 0;
-  strong:{
-    font-weight: 600;
-  }
-`
 
 export default App;
